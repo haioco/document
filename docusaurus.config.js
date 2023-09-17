088e82e50c7663123bcac68ff99c9e36e626e2be
Ali Zowghi <alizowghi@gmail.com>
2023-09-17 15:30:20 +0330

    Edit url ...

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,7 +61,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/haioco/document',
+            'https://github.com/haioco/document/tree/main',
         },
         blog: false, // Optional: disable the blog plugin
         // blog: {
